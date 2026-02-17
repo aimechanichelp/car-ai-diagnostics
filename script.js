422a754b4af3a286279ae367954a21bae8b5304c
aimechanichelp <mevoicimp@gmail.com>
2026-02-17 09:49:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyNow() {
+  // Replace this later with your real Stripe payment link
+  window.location.href = "https://buy.stripe.com/test_payment_link";
+}
+
+function submitIssue() {
+  alert("Your issue was submitted! You will receive a diagnosis by email.");
+}
